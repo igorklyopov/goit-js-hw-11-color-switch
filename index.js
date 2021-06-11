a1475c5b28775f7077d4c80b2af7dedb5d48f533
Igor Klyopov <shoot8202@gmail.com>
2021-06-11 18:40:59 +0300

    adds code improvement

--- index.js
+++ index.js
@@ -7,24 +7,25 @@ const colors = [
   "#795548",
 ];
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
 const bodyRef = document.querySelector("body");
 const startButtonRef = document.querySelector("[data-action='start']");
 const stopButtonRef = document.querySelector("[data-action='stop']");
+const CHANGE_COLORS_DELAY = 1000;
+let changesBodyColorAtIntervals = null;
 
 startButtonRef.addEventListener("click", onStartButtonClick);
 stopButtonRef.addEventListener("click", onStopButtonClick);
 
-let changesBodyColorAtIntervals = null;
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const randomColorIndex = () => randomIntegerFromInterval(0, colors.length - 1);
 
 function onStartButtonClick() {
   changesBodyColorAtInterval = setInterval(() => {
-    bodyRef.style.backgroundColor =
-      colors[randomIntegerFromInterval(0, colors.length)];
-  }, 1000);
+    bodyRef.style.backgroundColor = colors[randomColorIndex()];
+  }, CHANGE_COLORS_DELAY);
 
   startButtonRef.setAttribute("disabled", "disabled");
 }
